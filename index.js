faee657a33a9d0692a974d506cbaa9add8bac130
saharsh <saharshdc@gmail.com>
2022-10-12 21:02:53 -0400

    connected surveys

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const team = []
+
 const questions = [
     {
       type: 'input',
       message: 'Please enter Team Manager name',
-      name: 'manager',
+      name: 'name',
     },
     {
         type: 'input',
@@ -24,76 +26,89 @@ const questions = [
       {
         type: 'input',
         message: 'Please enter Office Number',
-        name: 'officenumber',
-      },
-      {
-        type: 'list',
-        message: 'Please select next team member type',
-        choices: ['Engineer','Intern', 'Finished Building team'],
-        name: 'next',
-      },
+        name: 'officeNumber',
+      }
   ]
 
-  const engQuestions =[ 
+  const empQuestions =[ 
     {
-    type: 'input',
-    message: "Please enter the engineer's Name.",
-    name: 'name',
-  },
-  {
-    type: 'input',
-    message: "Please enter the engineer's email address.",
-    name: 'email',
-  },
-  {
-    type: 'input',
-    message: "Please enter the engineer's ID#.",
-    name: 'id',
-  },
-  {
-    type: 'input',
-    message: "Please enter the engineer's GitHub username.",
-    name: 'github',
-  },
-]
+        type: 'list',
+        message: "Choose Next employee's role",
+        choices: ['Engineer', 'Intern', 'Finished Building Team'],
+        name: 'role'
+      },
+      {
+        type: 'input',
+        message: "Please enter the team member's name.",
+        name: 'name'
+      },
+      {
+        type: 'input',
+        message: "Please enter team member's ID#.",
+        name: 'id'
+      },
+      {
+        type: 'input',
+        message: "Please enter team member's Email address.",
+        name: 'email'
+      },
+      {
+        type: 'input',
+        message: "Please enter team member's Github:",
+        when: input => input.role === 'Engineer',
+        name: 'github',
+      },
+      {
+        type: 'input',
+        message: "Intern's School:",
+        when: input => input.role === 'Intern',
+        name: 'school',
+      },
+      // {
+      //   type: 'confirm',
+      //   message: 'Would you like to add another team member?',
+      //   default: false,
+      //   name: 'addEmployee',
+      // }
+    ]
 
-  const intQuestions =[ 
-    {
-        type: 'input',
-        message: "Please enter the intern's Name.",
-        name: 'name',
-      },
-      {
-        type: 'input',
-        message: "Please enter the intern's email address.",
-        name: 'email',
-      },
-      {
-        type: 'input',
-        message: "Please enter the intern's ID#.",
-        name: 'id',
-       },
-       {
-    type: 'input',
-    message: "Please enter the intern's School.",
-    name: 'school',
-  },
-  {
-    type: 'list',
-    message: 'Please select next team member type',
-    choices: ['Engineer','Intern', 'Finished Building team'],
-    name: 'next',
-  },
-]
 
   function init() {
     inquirer.prompt(questions)
     .then
     (function (answers){
-        const manager = new Manager (answers.manager, answers.id, answers.email, answers.officenumber)
-        console.log(answers)
-    }
+      const { name, id, email, officeNumber } = answers;
+      const manager = new Manager(name, id, email, officeNumber);
+        team.push(manager)
+        console.log(team)
+        buildTeam()
+          // .then
+          // buildTeam()
+            }
     )
   }
 
+  function buildTeam(){
+    inquirer.prompt(empQuestions)
+          .then
+    (function (answers){      
+      if (answers.role='Engineer'){
+        const { name, id, email, github } = answers;
+        const engineer = new Engineer(name, id, email, github);
+          team.push(engineer)
+          console.log(team)
+      }
+      else if (answers.role='Intern'){
+        const { name, id, email, school } = answers;
+        const intern = new Intern(name, id, email, school);
+          team.push(intern)
+          console.log(team)
+      }
+      else if (answers.role='Finished Building Team'){
+        console.log("Team Built, go home now")
+      }})
+      
+      }
+
   init()
+  // .then(buildTeam)
